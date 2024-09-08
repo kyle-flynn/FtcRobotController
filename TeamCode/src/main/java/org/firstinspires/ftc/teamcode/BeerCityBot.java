e67f82241f0c15cae2b4e2627827fe137cfc5793
Kyle Flynn <kyle.flynn5798@yahoo.com>
2024-09-08 16:01:00 -0400

    Working robot v1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BeerCityBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BeerCityBot.java
@@ -32,8 +32,11 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcore.external.JavaUtil;
 
 @TeleOp(name="Beer City Bot", group="Linear OpMode")
 public class BeerCityBot extends LinearOpMode {
@@ -44,10 +47,22 @@ public class BeerCityBot extends LinearOpMode {
     private final double LIFT_POWER = 0.5;
 
     // [INTAKE] Constant declarations
-    private final int INTAKE_ARM_HOME_POSITION = 0;
-    private final int INTAKE_ARM_DOWN_POSITION = -8000;
-    private final double INTAKE_ARM_POWER = 0.6;
+    private final int INTAKE_ARM_HOME_POSITION = -2000;
+    private final int INTAKE_ARM_DOWN_POSITION = -10200;
+    private final int INTAKE_ARM_HAND_OFF_POSITION = -4000;
+    private final int INTAKE_ARM_SCORING_POSITION = -5000;
+    private final double INTAKE_ARM_POWER = 1.0;
     private final double INTAKE_SERVO_POWER = -0.5;
+
+    // [BUCKET] Constant declarations
+    private final double BUCKET_HOME_POSITION = 0.2;
+    private final double BUCKET_SCORING_POSITION = 0.8;
+    private final double BUCKET_HAND_OFF_POSITION = 0.75;
+
+    // [CLIMB] Constant declarations
+    private final int CLIMB_HOME_POSITION = 0;
+    private final int CLIMB_MAX_POSITION = -27500;
+    private final double CLIMB_POWER = 1.0;
 
     // Robot state declarations
     private enum RobotState {
@@ -58,7 +73,12 @@ public class BeerCityBot extends LinearOpMode {
         HIGH_GOAL
     }
     private RobotState robotState;
-    private boolean robotStateChangeRequested;
+    private int lastStateChangeButton;
+    private boolean isScoring;
+    private boolean isClimbing;
+    private boolean isUsingOverride;
+    private boolean robotStateOverride;
+    private boolean liftLimitReached;
 
     // Motor declarations
     private DcMotor leftFront;
@@ -67,17 +87,21 @@ public class BeerCityBot extends LinearOpMode {
     private DcMotor rightBack;
     private DcMotor lift;
     private DcMotor intakeArm;
+    private DcMotor climbArm;
 
-    // Motor stall declarations
-    private boolean isLiftStalling;
-    private double liftStallTime;
-    private int liftEncoderDrift;
-    private boolean isArmStalling;
-    private double armStallTime;
-    private int armEncoderDrift;
-
-    // Servo Declarations
+    // Servo declarations
     private CRServo intakeServo;
+    private Servo bucketServo;
+
+    // DigitalInput declarations
+    private TouchSensor liftLimit;
+
+    // Drive modifiers
+    private double speed;
+    private double forward;
+    private double strafe;
+    private double denominator;
+    private double turn;
 
     private ElapsedTime time;
 
@@ -92,13 +116,13 @@ public class BeerCityBot extends LinearOpMode {
 
         // Setup our robot state
         robotState = RobotState.DRIVE;
-        robotStateChangeRequested = false;
+        lastStateChangeButton = -1;
+        isScoring = false;
+        isClimbing = false;
+        liftLimitReached = true;
 
-        // Initialize stall variables
-        isLiftStalling = false;
-        liftStallTime = 0.0;
-        isArmStalling = false;
-        armStallTime = 0.0;
+        // Initialize drive modifiers
+        speed = 0.7;
 
         // Map all of our declarations to hardware
         mapHardware();
@@ -114,8 +138,10 @@ public class BeerCityBot extends LinearOpMode {
 
         // Run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            updateRobotState();
-            checkForStallingMotors();
+            readDriverInputs();
+            readOperatorInputs();
+            drive();
+            updateLift();
             updateTelemetry();
         }
     }
@@ -129,30 +155,21 @@ public class BeerCityBot extends LinearOpMode {
         lift = hardwareMap.get(DcMotor.class, "lift");
         intakeArm = hardwareMap.get(DcMotor.class, "intakeArm");
 
+        climbArm = hardwareMap.get(DcMotor.class, "climbArm");
+
         intakeServo = hardwareMap.get(CRServo.class, "intakeServo");
+        bucketServo = hardwareMap.get(Servo.class, "bucketServo");
+
+        liftLimit = hardwareMap.get(TouchSensor.class, "liftLimit");
+
+        // Invert right side motors since they are mirrored on the chassis
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
 
         // For our motors with encoders, reset their positions to 0.
         lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         intakeArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    }
-
-    private void updateRobotState() {
-        waitForRobotStateChange();
-        updateRobotSystemServos();
-    }
-
-    private void waitForRobotStateChange() {
-        if (gamepad1.a && !robotStateChangeRequested) {
-            robotStateChangeRequested = true;
-            advanceRobotState(true);
-            updateRobotSystemPositions();
-        } else if (gamepad1.b && !robotStateChangeRequested) {
-            robotStateChangeRequested = true;
-            advanceRobotState(false);
-            updateRobotSystemPositions();
-        } else if ((!gamepad1.a || !gamepad1.b) && robotStateChangeRequested) {
-            robotStateChangeRequested = false;
-        }
+        climbArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
     private void advanceRobotState(boolean advance) {
@@ -178,7 +195,6 @@ public class BeerCityBot extends LinearOpMode {
     }
 
     private void updateRobotSystemPositions() {
-        // TODO - Incorporate drift values, needs additional testing.
         switch (robotState) {
             case DRIVE:
                 intakeArm.setTargetPosition(INTAKE_ARM_HOME_POSITION);
@@ -189,15 +205,15 @@ public class BeerCityBot extends LinearOpMode {
                 lift.setTargetPosition(LIFT_HOME_POSITION);
                 break;
             case HAND_OFF:
-                intakeArm.setTargetPosition(INTAKE_ARM_HOME_POSITION);
+                intakeArm.setTargetPosition(INTAKE_ARM_HAND_OFF_POSITION);
                 lift.setTargetPosition(LIFT_HOME_POSITION);
                 break;
             case LOW_GOAL:
-                intakeArm.setTargetPosition(INTAKE_ARM_HOME_POSITION);
+                intakeArm.setTargetPosition(INTAKE_ARM_SCORING_POSITION);
                 lift.setTargetPosition(LIFT_LOW_POSITION);
                 break;
             case HIGH_GOAL:
-                intakeArm.setTargetPosition(INTAKE_ARM_HOME_POSITION);
+                intakeArm.setTargetPosition(INTAKE_ARM_SCORING_POSITION);
                 lift.setTargetPosition(LIFT_HIGH_POSITION);
                 break;
             default:
@@ -214,12 +230,15 @@ public class BeerCityBot extends LinearOpMode {
         switch (robotState) {
             case DRIVE:
                 intakeServo.setPower(0.0);
+                bucketServo.setPosition(BUCKET_HOME_POSITION);
                 break;
             case INTAKE:
                 intakeServo.setPower(INTAKE_SERVO_POWER);
+                bucketServo.setPosition(BUCKET_HOME_POSITION);
                 break;
             case HAND_OFF:
                 intakeServo.setPower(0.0);
+                bucketServo.setPosition(BUCKET_HAND_OFF_POSITION);
                 break;
             case LOW_GOAL:
                 intakeServo.setPower(0.0);
@@ -232,47 +251,127 @@ public class BeerCityBot extends LinearOpMode {
         }
     }
 
-    private void checkForStallingMotors() {
-        int liftError = Math.abs(lift.getTargetPosition() - lift.getCurrentPosition());
-        int armError = Math.abs(intakeArm.getTargetPosition() - intakeArm.getCurrentPosition());
+    private void readDriverInputs() {
+        if (gamepad1.dpad_up) {
+            speed = 1;
+        } else if (gamepad1.dpad_left || gamepad1.dpad_right) {
+            speed = 0.7;
+        } else if (gamepad1.dpad_down) {
+            speed = 0.4;
+        }
+        forward = -gamepad1.left_stick_y * speed;
+        strafe = gamepad1.left_stick_x * speed;
+        turn = (gamepad1.right_stick_x + (gamepad1.right_trigger - gamepad1.left_trigger)) * speed;
+        denominator = JavaUtil.maxOfList(JavaUtil.createListWith(1, Math.abs(forward) + strafe + turn));
+    }
 
-        if (liftError <= 100 && liftStallTime <= 0.0) {
-            // Now we've got the moment the lift became within 100 ticks of its target
-            liftStallTime = time.time();
+    private void drive() {
+        leftFront.setPower((forward + strafe + turn) / denominator);
+        leftBack.setPower((forward - (strafe - turn)) / denominator);
+        rightFront.setPower((forward - (strafe + turn)) / denominator);
+        rightBack.setPower((forward + (strafe - turn)) / denominator);
+    }
+
+    private void readOperatorInputs() {
+        if (gamepad2.a && lastStateChangeButton != 0) {
+            lastStateChangeButton = 0;
+            advanceRobotState(true);
+            updateRobotSystemPositions();
+            updateRobotSystemServos();
+        } else if (gamepad2.b && lastStateChangeButton != 1) {
+            lastStateChangeButton = 1;
+            advanceRobotState(false);
+            updateRobotSystemPositions();
+            updateRobotSystemServos();
+        } else if (gamepad2.x && lastStateChangeButton != 2) {
+            lastStateChangeButton = 2;
+            robotState = RobotState.DRIVE;
+            updateRobotSystemPositions();
+            updateRobotSystemServos();
+        } else if (!gamepad2.a && lastStateChangeButton == 0) {
+            lastStateChangeButton = -1;
+        } else if (!gamepad2.b && lastStateChangeButton == 1) {
+            lastStateChangeButton = -1;
+        } else if (!gamepad2.x && lastStateChangeButton == 2) {
+            lastStateChangeButton = -1;
         }
 
-        // Now we check if the error is still within 100 ticks, and if the elapsed time has exceeded the boundaries
-        if ((time.time() - liftStallTime) >= 2.0 && liftError <= 100) {
-            // The motor has stalled, set the target position to the new position
-            liftEncoderDrift = lift.getTargetPosition() - lift.getCurrentPosition();
-            lift.setTargetPosition(lift.getCurrentPosition());
-            liftStallTime = 0.0;
+        if (gamepad2.y && !isScoring) {
+            boolean isServoHome = bucketServo.getPosition() == BUCKET_HOME_POSITION;
+            bucketServo.setPosition(isServoHome  ? BUCKET_SCORING_POSITION : BUCKET_HOME_POSITION);
+            isScoring = true;
+        } else if (!gamepad2.y && isScoring) {
+            isScoring = false;
         }
 
-        if (armError <= 100 && armStallTime <= 0.0) {
-            // Now we've got the moment the lift became within 100 ticks of its target
-            armStallTime = time.time();
+        if (gamepad2.right_bumper && !isClimbing) {
+            climbArm.setTargetPosition(CLIMB_MAX_POSITION);
+            climbArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            climbArm.setPower(CLIMB_POWER);
+            isClimbing = true;
+        } else if (!gamepad2.right_bumper && isClimbing) {
+            isClimbing = false;
         }
 
-        // Now we check if the error is still within 100 ticks, and if the elapsed time has exceeded the boundaries
-        if ((time.time() - armStallTime) >= 2.0 && armError <= 100) {
-            // The motor has stalled, set the target position to the new position
-            armEncoderDrift = intakeArm.getTargetPosition() - lift.getCurrentPosition();
-            intakeArm.setTargetPosition(intakeArm.getCurrentPosition());
-            armStallTime = 0.0;
+        if (gamepad2.left_bumper && !isClimbing) {
+            climbArm.setTargetPosition(CLIMB_HOME_POSITION);
+            climbArm.setPower(CLIMB_POWER);
+            climbArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            isClimbing = true;
+        } else if (!gamepad2.left_bumper && isClimbing) {
+            isClimbing = false;
+        }
+
+        if (gamepad2.start && !isUsingOverride) {
+            isUsingOverride = true;
+            robotStateOverride = !robotStateOverride;
+            if (robotStateOverride) {
+                lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                intakeArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                climbArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            }
+        } else if (!gamepad2.start && isUsingOverride) {
+            isUsingOverride = false;
+        }
+
+        if (robotStateOverride) {
+            handleOverrides();
+        }
+    }
+
+    /**
+     * Override method for the robot's state. Code in this method should only be executed
+     * if the robot is in override mode, otherwise the robot should be controlled by the state machine.
+     */
+    private void handleOverrides() {
+        lift.setPower(gamepad2.left_stick_y * -LIFT_POWER);
+        intakeArm.setPower(gamepad2.right_stick_y * -INTAKE_ARM_POWER);
+        climbArm.setPower((-gamepad2.left_trigger + gamepad2.right_trigger) * CLIMB_POWER);
+    }
+
+
+    private void updateLift() {
+        if (liftLimit.isPressed() && !liftLimitReached) {
+            liftLimitReached = true;
+            lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        } else if (!liftLimit.isPressed() && liftLimitReached) {
+            liftLimitReached = false;
         }
     }
 
     private void updateTelemetry() {
-        telemetry.addData("Robot State", robotState.toString());
+        telemetry.addData("Robot State", robotStateOverride ? "OVERRIDE" : robotState.toString());
         telemetry.addLine();
         telemetry.addData("Lift Position", lift.getCurrentPosition());
         telemetry.addData("Lift Target", lift.getTargetPosition());
-        telemetry.addData("Lift Stall Time", liftStallTime);
+        telemetry.addData("Lift Limit", liftLimit.isPressed());
         telemetry.addLine();
         telemetry.addData("Intake Position", intakeArm.getCurrentPosition());
         telemetry.addData("Intake Target", intakeArm.getTargetPosition());
-        telemetry.addData("Intake Stall Time", armStallTime);
+        telemetry.addLine();
+        telemetry.addData("Bucket Servo", bucketServo.getPosition());
+        telemetry.addLine();
+        telemetry.addData("Climb", climbArm.getCurrentPosition());
         telemetry.update();
     }
 }
